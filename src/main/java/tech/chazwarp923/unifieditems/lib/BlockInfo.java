90ff250935b18b698b83b7798921e7579f830872
Chaz <chazwarp923@icloud.com>
2016-07-12 15:32:37 -0400

    Remove all traces of Rubber and the rubber tap

--- src/main/java/tech/chazwarp923/unifieditems/lib/BlockInfo.java
+++
@@ -1,6 +0,0 @@
-package tech.chazwarp923.unifieditems.lib;
-
-public class BlockInfo {
-
-	public static final String RUBBER_TAP_UNLOCALIZED_NAME = "rubberTap";
-}
--- src/main/java/tech/chazwarp923/unifieditems/lib/ItemInfo.java
+++
@@ -1,8 +0,0 @@
-package tech.chazwarp923.unifieditems.lib;
-
-public class ItemInfo {
-
-	public static final String RAW_RUBBER_UNLOCALIZED_NAME = "rawRubber";
-	public static final String INGOT_RUBBER_UNLOCALIZED_NAME = "ingotRubber";
-	public static final String MORTAR_AND_PESTLE_UNLOCALIZED_NAME = "mortarAndPestle";
-}
