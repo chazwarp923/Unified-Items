abeaca20e98a9a7b38352580e001ef3ebb1afc28
Chaz <chazwarp923@icloud.com>
2016-07-13 17:15:48 -0400

    Update to latest JEI, crafting and smelting recipes plus preliminary work for world gen

--- src/main/java/tech/chazwarp923/unifieditems/crafting/Shapeless.java
+++ src/main/java/tech/chazwarp923/unifieditems/crafting/Shapeless.java
@@ -1,44 +1,35 @@
 package tech.chazwarp923.unifieditems.crafting;
 
+import java.util.Map;
+
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
+import tech.chazwarp923.unifieditems.block.UIBlockOre;
+import tech.chazwarp923.unifieditems.block.UIBlocks;
+import tech.chazwarp923.unifieditems.item.UIItemIngot;
 import tech.chazwarp923.unifieditems.item.UIItems;
 
 public class Shapeless {
 
 	public static void preInit() {
 
+		// Adds The Recipes for Ores to Dusts
+		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(UIItems.dusts.get("Iron"), 2), new Object[] { "craftingMortarAndPestle", "oreIron" }));
+		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(UIItems.dusts.get("Gold"), 2), new Object[] { "craftingMortarAndPestle", "oreGold" }));
+		for(Map.Entry<String, UIBlockOre> block : UIBlocks.ores.entrySet()) {
+			GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(UIItems.dusts.get(block.getKey()), 2), new Object[] { "craftingMortarAndPestle","ore" + block.getKey() }));
+		}
+		
 		// Adds the recipe for bronze
-		GameRegistry.addShapelessRecipe(new ItemStack(UIItems.ingots.get("Bronze"), 4), new ItemStack(UIItems.ingots.get("Copper")), new ItemStack(UIItems.ingots.get("Copper")), new ItemStack(UIItems.ingots.get("Copper")), new ItemStack(UIItems.ingots.get("Tin")));
 		GameRegistry.addShapelessRecipe(new ItemStack(UIItems.dusts.get("Bronze"), 4), new ItemStack(UIItems.dusts.get("Copper")), new ItemStack(UIItems.dusts.get("Copper")), new ItemStack(UIItems.dusts.get("Copper")), new ItemStack(UIItems.dusts.get("Tin")));
 
 		// Adds the recipes for all the storage blocks
-		/*GameRegistry.addShapelessRecipe(new ItemStack(UIBlocks.blockCopper), new ItemStack(UIItems.ingotCopper), new ItemStack(UIItems.ingotCopper), new ItemStack(UIItems.ingotCopper), new ItemStack(UIItems.ingotCopper), new ItemStack(UIItems.ingotCopper), new ItemStack(UIItems.ingotCopper), new ItemStack(UIItems.ingotCopper), new ItemStack(UIItems.ingotCopper), new ItemStack(UIItems.ingotCopper));
-		GameRegistry.addShapelessRecipe(new ItemStack(UIBlocks.blockTin), new ItemStack(UIItems.ingotTin), new ItemStack(UIItems.ingotTin), new ItemStack(UIItems.ingotTin), new ItemStack(UIItems.ingotTin), new ItemStack(UIItems.ingotTin), new ItemStack(UIItems.ingotTin), new ItemStack(UIItems.ingotTin), new ItemStack(UIItems.ingotTin), new ItemStack(UIItems.ingotTin));
-		GameRegistry.addShapelessRecipe(new ItemStack(UIBlocks.blockBronze), new ItemStack(UIItems.ingotBronze), new ItemStack(UIItems.ingotBronze), new ItemStack(UIItems.ingotBronze), new ItemStack(UIItems.ingotBronze), new ItemStack(UIItems.ingotBronze), new ItemStack(UIItems.ingotBronze), new ItemStack(
-						UIItems.ingotBronze), new ItemStack(
-						UIItems.ingotBronze), new ItemStack(
-						UIItems.ingotBronze));
-		GameRegistry.addShapelessRecipe(new ItemStack(UIBlocks.blockSilver),
-				new ItemStack(UIItems.ingotSilver), new ItemStack(
-						UIItems.ingotSilver), new ItemStack(
-						UIItems.ingotSilver), new ItemStack(
-						UIItems.ingotSilver), new ItemStack(
-						UIItems.ingotSilver), new ItemStack(
-						UIItems.ingotSilver), new ItemStack(
-						UIItems.ingotSilver), new ItemStack(
-						UIItems.ingotSilver), new ItemStack(
-						UIItems.ingotSilver));
-		GameRegistry.addShapelessRecipe(new ItemStack(UIBlocks.blockLead),
-				new ItemStack(UIItems.ingotLead), new ItemStack(
-						UIItems.ingotLead), new ItemStack(UIItems.ingotLead),
-				new ItemStack(UIItems.ingotLead), new ItemStack(
-						UIItems.ingotLead), new ItemStack(UIItems.ingotLead),
-				new ItemStack(UIItems.ingotLead), new ItemStack(
-						UIItems.ingotLead), new ItemStack(UIItems.ingotLead));*/
+		for(Map.Entry<String, UIItemIngot> item : UIItems.ingots.entrySet()) {
+			GameRegistry.addShapelessRecipe(new ItemStack(UIBlocks.blocks.get(item.getKey())), new ItemStack(item.getValue()), new ItemStack(item.getValue()), new ItemStack(item.getValue()), new ItemStack(item.getValue()), new ItemStack(item.getValue()), new ItemStack(item.getValue()), new ItemStack(item.getValue()), new ItemStack(item.getValue()), new ItemStack(item.getValue()));
+		}
 
 		// Adds the recipe for the "Mortar And Pestle"
 		GameRegistry.addShapelessRecipe(new ItemStack(UIItems.mortarAndPestle), new ItemStack(Items.BOWL), new ItemStack(Blocks.COBBLESTONE));
@@ -46,14 +37,6 @@ public class Shapeless {
 		// Adds a recipe for flint
 		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Items.FLINT, 1), new Object[] { "blockGravel","craftingMortarAndPestle" }));
 
-		/*// Adds The Recipes for Ores to Dusts
-		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(UIItems.dustIron, 2), new Object[] { "oreIron","craftingMortarAndPestle" }));
-		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(UIItems.dustGold, 2), new Object[] { "oreGold","craftingMortarAndPestle" }));
-		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(UIItems.dustCopper, 2), new Object[] { "oreCopper","craftingMortarAndPestle" }));
-		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(UIItems.dustTin, 2), new Object[] { "oreTin","craftingMortarAndPestle" }));
-		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(UIItems.dustSilver, 2), new Object[] { "oreSilver","craftingMortarAndPestle" }));
-		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(UIItems.dustLead, 2), new Object[] { "oreLead","craftingMortarAndPestle" }));
-		*/
 		// Miscellaneous Recipes
 		GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(Blocks.TORCH, 2), new Object[] { "stickWood","craftingFlintAndSteel" }));
 	}
--- src/main/java/tech/chazwarp923/unifieditems/material/MaterialRegistry.java
+++ src/main/java/tech/chazwarp923/unifieditems/material/MaterialRegistry.java
@@ -10,37 +10,37 @@ public enum MaterialRegistry {
 	COAL(MaterialType.DUST, "Coal"),
 	IRON(MaterialType.DUST, "Iron"),
 	GOLD(MaterialType.DUST, "Gold"),
-	COPPER(MaterialType.GENERIC, "Copper", 2F, HarvestLevel.STONE),
-	TIN(MaterialType.GENERIC, "Tin", 2F, HarvestLevel.STONE),
+	COPPER(MaterialType.GENERIC, "Copper", 2F, HarvestLevel.STONE, 40, 75, 8, 10, 1),
+	TIN(MaterialType.GENERIC, "Tin", 2F, HarvestLevel.STONE, 20, 55, 8, 10, 1),
 	BRONZE(MaterialType.ALLOY, "Bronze"),
-	SILVER(MaterialType.GENERIC, "Silver", 2F, HarvestLevel.DIAMOND),
-	LEAD(MaterialType.GENERIC, "Lead", 2F, HarvestLevel.DIAMOND),
-	NICKEL(MaterialType.GENERIC, "Nickel", 2F, HarvestLevel.IRON),
+	SILVER(MaterialType.GENERIC, "Silver", 2F, HarvestLevel.DIAMOND, 5, 30, 8, 10, 1),
+	LEAD(MaterialType.GENERIC, "Lead", 2F, HarvestLevel.DIAMOND, 10, 35, 8, 10, 1),
+	NICKEL(MaterialType.GENERIC, "Nickel", 2F, HarvestLevel.IRON, 5, 20, 4, 2, 1),
 	INVAR(MaterialType.ALLOY, "Invar"),
 	ELECTRUM(MaterialType.ALLOY, "Electrum"),
-	PLATINUM(MaterialType.GENERIC, "Platinum", 2F, HarvestLevel.DIAMOND),
-	MITHRIL(MaterialType.GENERIC, "Mithril", 2F, HarvestLevel.DIAMOND),
-    ALUMINUM(MaterialType.GENERIC, "Aluminum", 2F, HarvestLevel.STONE),
-    URANIUM(MaterialType.GENERIC, "Uranium", 2F, HarvestLevel.DIAMOND),
+	PLATINUM(MaterialType.GENERIC, "Platinum", 2F, HarvestLevel.DIAMOND, 5, 15, 2, 2, 1),
+	MITHRIL(MaterialType.GENERIC, "Mithril", 2F, HarvestLevel.DIAMOND, 5, 15, 2, 2, 1),
+    ALUMINUM(MaterialType.GENERIC, "Aluminum", 2F, HarvestLevel.STONE, 40, 85, 8, 12, 1),
+    URANIUM(MaterialType.GENERIC, "Uranium", 2F, HarvestLevel.DIAMOND, 5, 25, 2, 4, 1),
     STEEL(MaterialType.ALLOY, "Steel"),
-    COBALT(MaterialType.GENERIC, "Cobalt", 2F, HarvestLevel.DIAMOND),
-    ARDITE(MaterialType.GENERIC, "Ardite", 2F, HarvestLevel.DIAMOND),
-    IRIDIUM(MaterialType.GENERIC, "Iridium", 2F, HarvestLevel.DIAMOND);
+    COBALT(MaterialType.GENERIC, "Cobalt", 2F, HarvestLevel.DIAMOND, 5, 120, 3, 4, 2),
+    ARDITE(MaterialType.GENERIC, "Ardite", 2F, HarvestLevel.DIAMOND, 5, 120, 3, 4, 2),
+    IRIDIUM(MaterialType.GENERIC, "Iridium", 2F, HarvestLevel.DIAMOND, 5, 10, 1, 1, 1);
 	
 	final MaterialType type;
 	final String name;
 	final float hardness;
 	final HarvestLevel harvestLevel;
 	
-	MaterialRegistry(MaterialType types, String name, float hardness, HarvestLevel harvestLevel) {
-		this.type = types;
+	MaterialRegistry(MaterialType type, String name, float hardness, HarvestLevel harvestLevel, int minY, int maxY, int perVein, int chunkDensity, int dimId) {
+		this.type = type;
 		this.name = name;
 		this.hardness = hardness;
 		this.harvestLevel = harvestLevel;
 	}
 	
-	MaterialRegistry(MaterialType types, String name) {
-		this.type = types;
+	MaterialRegistry(MaterialType type, String name) {
+		this.type = type;
 		this.name = name;
 		this.hardness = -1;
 		this.harvestLevel = HarvestLevel.WOOD;
