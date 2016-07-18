58f43b0be5def0fa093ffa2abe9114bf59188a58
Chaz <chazwarp923@icloud.com>
2016-07-18 00:58:33 -0400

    Further testing to try and fix #2

--- src/main/java/tech/chazwarp923/unifieditems/event/ItemCraftedEventHandler.java
+++ src/main/java/tech/chazwarp923/unifieditems/event/ItemCraftedEventHandler.java
@@ -4,11 +4,14 @@
 
 package tech.chazwarp923.unifieditems.event;
 
+import org.apache.logging.log4j.Level;
+
 import net.minecraft.init.Items;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.ItemCraftedEvent;
+import tech.chazwarp923.unifieditems.UnifiedItems;
 import tech.chazwarp923.unifieditems.item.ItemMortarAndPestle;
 import tech.chazwarp923.unifieditems.item.UIItems;
 
@@ -18,16 +21,17 @@ public class ItemCraftedEventHandler {
 	public void itemCrafted(ItemCraftedEvent event) {
 		
 		IInventory craftingGrid = event.craftMatrix;
+		int slot = 0;
 		
 		//Helps the torch recipe
-		for (int i = 0; i < craftingGrid.getSizeInventory(); i++) {
+		/*for (int i = 0; i < craftingGrid.getSizeInventory(); i++) {
 			if (craftingGrid.getStackInSlot(i) != null) {
 				if(craftingGrid.getStackInSlot(i).getItem() == Items.FLINT_AND_STEEL) {
 					ItemStack oldStack = craftingGrid.getStackInSlot(i);
 					ItemStack newStack = new ItemStack(Items.FLINT_AND_STEEL, 2, oldStack.getItemDamage() + 1);
 					if(newStack.getItemDamage() == newStack.getMaxDamage()) {
 						newStack = null;
-						//Everything inside this for loop fixes a duplication bug introduced somehwere between mc1.8 and 1.10, very important do not remove if copying
+						//Everything inside this for loop fixes a duplication bug introduced somewhere between mc1.8 and 1.10, very important do not remove if copying
 						for(int j = 0; j < craftingGrid.getSizeInventory(); j++) {
 							if(craftingGrid.getStackInSlot(j) != null) {
 								if(!(craftingGrid.getStackInSlot(j).getItem() instanceof ItemMortarAndPestle)) {
@@ -44,7 +48,7 @@ public class ItemCraftedEventHandler {
 					craftingGrid.setInventorySlotContents(i, newStack);
 				}
 			}
-		}
+		}*/
 
 		//Helps the ore and flint recipes
 		for (int i = 0; i < craftingGrid.getSizeInventory(); i++) {
@@ -54,21 +58,31 @@ public class ItemCraftedEventHandler {
 					ItemStack newStack = new ItemStack(UIItems.mortarAndPestle, 2, oldStack.getItemDamage() + 1);
 					if(newStack.getItemDamage() == newStack.getMaxDamage()) {
 						newStack = null;
-						//Everything inside this for loop fixes a duplication bug introduced somehwere between mc1.8 and 1.10, very important do not remove if copying
+						//Everything inside this for loop fixes a duplication bug introduced somewhere between mc1.8 and 1.10, very important do not remove if copying
 						for(int j = 0; j < craftingGrid.getSizeInventory(); j++) {
 							if(craftingGrid.getStackInSlot(j) != null) {
-								if(!(craftingGrid.getStackInSlot(j).getItem() instanceof ItemMortarAndPestle)) {
+								if(j != i) {
 									ItemStack oldOre = craftingGrid.getStackInSlot(j);
-									ItemStack newOre = new ItemStack(oldOre.getItem(), ((int)oldOre.stackSize / 2) + 1);
-									if(newOre.stackSize == 1) {
+									UnifiedItems.logger.log(Level.INFO, "Old Ore Stack: " + oldOre);
+									int stackSize = oldOre.stackSize / 2;
+									UnifiedItems.logger.log(Level.INFO, "Assumed Stack Size: " + stackSize);
+									if(oldOre.stackSize == 1)
+										stackSize = 0;
+									UnifiedItems.logger.log(Level.INFO, "Corrected Stack Size: " + stackSize);
+									ItemStack newOre = new ItemStack(oldOre.getItem(), stackSize);
+									UnifiedItems.logger.log(Level.INFO, "New Ore Stack: " + newOre);
+									if(newOre.stackSize == 0) {
 										newOre = null;
 									}
 									craftingGrid.setInventorySlotContents(j, newOre);
+									UnifiedItems.logger.log(Level.INFO, "Slot Contents: " + craftingGrid.getStackInSlot(j));
+									slot = j;
 								}
 							}
 						}
 					}
 					craftingGrid.setInventorySlotContents(i, newStack);
+					UnifiedItems.logger.log(Level.INFO, "Slot Contents: " + craftingGrid.getStackInSlot(slot));
 				}
 			}
 		}
