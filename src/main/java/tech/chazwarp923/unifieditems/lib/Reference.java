6d2da16242232e72fcadbc69ee0e27963352572e
Chaz <chazwarp923@icloud.com>
2016-07-23 12:39:03 -0400

    Bump version number

--- src/main/java/tech/chazwarp923/unifieditems/lib/Reference.java
+++ src/main/java/tech/chazwarp923/unifieditems/lib/Reference.java
@@ -7,7 +7,7 @@ public class Reference {
 
 	public static final String MOD_ID = "UnifiedItems";
 	public static final String MOD_NAME = "Unified Items";
-	public static final String VERSION = "1.0.5";
+	public static final String VERSION = "1.0.6";
 	public static final String TEXTURE_LOC = MOD_ID.toLowerCase();
 	public static final String RESOURCE_PREFIX = TEXTURE_LOC + ":";
 }
