feb25c86c8ed3757741c7dc75d3f3f13737af781
Chaz <chazwarp923@icloud.com>
2017-01-19 17:49:06 -0500

    Modify mining level of silver and lead, closes #5

--- src/main/java/tech/chazwarp923/unifieditems/material/Material.java
+++ src/main/java/tech/chazwarp923/unifieditems/material/Material.java
@@ -13,24 +13,24 @@ public enum Material {
 	DIAMOND(MaterialType.DUST, "Diamond"),
 	COPPER(MaterialType.GENERIC, "Copper", 2F, HarvestLevel.STONE, 40, 75, 8, 10, 0),
 	TIN(MaterialType.GENERIC, "Tin", 2F, HarvestLevel.STONE, 20, 55, 8, 10, 0),
-	BRONZE(MaterialType.ALLOY, "Bronze"),
-	SILVER(MaterialType.GENERIC, "Silver", 2F, HarvestLevel.DIAMOND, 5, 30, 8, 10, 0),
-	LEAD(MaterialType.GENERIC, "Lead", 2F, HarvestLevel.DIAMOND, 10, 35, 8, 10, 0),
+	BRONZE(MaterialType.ALLOY, "Bronze", 2F, HarvestLevel.IRON),
+	SILVER(MaterialType.GENERIC, "Silver", 2F, HarvestLevel.IRON, 5, 30, 8, 10, 0),
+	LEAD(MaterialType.GENERIC, "Lead", 2F, HarvestLevel.IRON, 10, 35, 8, 10, 0),
 	NICKEL(MaterialType.GENERIC, "Nickel", 2F, HarvestLevel.IRON, 5, 20, 4, 2, 0),
-	INVAR(MaterialType.ALLOY, "Invar"),
-	ELECTRUM(MaterialType.ALLOY, "Electrum"),
+	INVAR(MaterialType.ALLOY, "Invar", 2F, HarvestLevel.IRON),
+	ELECTRUM(MaterialType.ALLOY, "Electrum", 2F, HarvestLevel.IRON),
 	OBSIDIAN(MaterialType.DUST, "Obsidian"),
 	PLATINUM(MaterialType.GENERIC, "Platinum", 2F, HarvestLevel.DIAMOND, 5, 15, 2, 2, 0),
 	MITHRIL(MaterialType.GENERIC, "Mithril", 2F, HarvestLevel.DIAMOND, 5, 15, 2, 2, 0),
     ALUMINUM(MaterialType.GENERIC, "Aluminum", 2F, HarvestLevel.STONE, 40, 85, 8, 12, 0),
     URANIUM(MaterialType.GENERIC, "Uranium", 2F, HarvestLevel.DIAMOND, 5, 25, 2, 4, 0),
-    STEEL(MaterialType.ALLOY, "Steel"),
+    STEEL(MaterialType.ALLOY, "Steel", 2F, HarvestLevel.IRON),
     COBALT(MaterialType.GENERIC, "Cobalt", 2F, HarvestLevel.DIAMOND, 5, 120, 3, 4, 1),
     ARDITE(MaterialType.GENERIC, "Ardite", 2F, HarvestLevel.DIAMOND, 5, 120, 3, 4, 1),
     IRIDIUM(MaterialType.GENERIC, "Iridium", 2F, HarvestLevel.DIAMOND, 5, 10, 1, 1, 0),
 	RUBY(MaterialType.GENERIC_GEM, "Ruby", 2F, HarvestLevel.IRON, 10, 60, 6, 3, 0),
 	SAPPHIRE(MaterialType.GENERIC_GEM, "Sapphire", 2F, HarvestLevel.IRON, 10, 60, 6, 3, 0),
-	BRASS(MaterialType.ALLOY, "Brass"),
+	BRASS(MaterialType.ALLOY, "Brass", 2F, HarvestLevel.IRON),
 	ZINC(MaterialType.GENERIC, "Zinc", 2F, HarvestLevel.IRON, 20, 55, 7, 10, 0);
 	
 	public final MaterialType type;
@@ -53,6 +53,18 @@ public enum Material {
 		this.veinSize = veinSize;
 		this.chunkDensity = chunkDensity;
 		this.dimId = dimId;
+	}
+	
+	Material(MaterialType type, String name, float hardness, HarvestLevel harvestLevel) {
+		this.type = type;
+		this.name = name;
+		this.hardness = hardness;
+		this.harvestLevel = harvestLevel;
+		this.minY = 0;
+		this.maxY = 0;
+		this.veinSize = 0;
+		this.chunkDensity = 0;
+		this.dimId = 0;
 	}
 	
 	Material(MaterialType type, String name) {
