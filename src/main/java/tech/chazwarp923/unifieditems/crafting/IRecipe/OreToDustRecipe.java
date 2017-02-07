61fa4d800c3be91738cb759f09565886f29c0b74
Chaz <chazwarp923@icloud.com>
2017-02-07 10:19:55 -0500

    Fixes the Mortar and Pestle on 1.10

--- src/main/java/tech/chazwarp923/unifieditems/crafting/IRecipe/OreToDustRecipe.java
+++ src/main/java/tech/chazwarp923/unifieditems/crafting/IRecipe/OreToDustRecipe.java
@@ -1,18 +1,17 @@
 package tech.chazwarp923.unifieditems.crafting.IRecipe;
 
+import mcjty.lib.compat.CompatIRecipe;
+import mcjty.lib.tools.ItemStackList;
 import mcjty.lib.tools.ItemStackTools;
 import net.minecraft.init.Blocks;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.util.NonNullList;
 import net.minecraft.world.World;
 import tech.chazwarp923.unifieditems.block.UIBlocks;
-import tech.chazwarp923.unifieditems.item.ItemMortarAndPestle;
 import tech.chazwarp923.unifieditems.item.UIItems;
 import tech.chazwarp923.unifieditems.material.Material;
 
-public class OreToDustRecipe implements IRecipe {
+public class OreToDustRecipe implements CompatIRecipe {
 
 	protected ItemStack input;
 	protected ItemStack output;
@@ -36,10 +35,10 @@ public class OreToDustRecipe implements IRecipe {
 	public boolean matches(InventoryCrafting inv, World worldIn) {
 		int invSize = inv.getSizeInventory();
 		for(int i = 0; i < invSize; i++) {
-			if(!inv.getStackInSlot(i).equals(ItemStackTools.getEmptyStack())) {
+			if(ItemStackTools.isValid(inv.getStackInSlot(i))) {
 				if(inv.getStackInSlot(i).getItem().equals(UIItems.mortarAndPestle)) {
 					for(int j = 0; j < invSize; j++) {
-						if(!inv.getStackInSlot(j).equals(ItemStackTools.getEmptyStack())) {
+						if(ItemStackTools.isValid(inv.getStackInSlot(j))) {
 							if(inv.getStackInSlot(j).getItem().equals(input.getItem())) {
 								return true;
 							}
@@ -67,20 +66,20 @@ public class OreToDustRecipe implements IRecipe {
 	}
 
 	@Override
-	public NonNullList<ItemStack> getRemainingItems(InventoryCrafting inv) {
-		NonNullList<ItemStack> nnl = NonNullList.<ItemStack>withSize(inv.getSizeInventory(), ItemStackTools.getEmptyStack());
+	public ItemStackList getRemainingItemsCompat(InventoryCrafting inv) {
+		ItemStackList grid = ItemStackList.create(inv.getSizeInventory());
 
-        for (int i = 0; i < nnl.size(); i++) {
+        for (int i = 0; i < grid.size(); i++) {
             ItemStack itemstack = inv.getStackInSlot(i);
             
-            if (!(itemstack.equals(ItemStackTools.getEmptyStack())) && itemstack.getItem() instanceof ItemMortarAndPestle) {
+            if (ItemStackTools.isValid(itemstack) && itemstack.getItem().equals(UIItems.mortarAndPestle)) {
             	itemstack = new ItemStack(UIItems.mortarAndPestle, 1, itemstack.getItemDamage() + 1);
                 if(itemstack.getItemDamage() == itemstack.getMaxDamage())
-                	ItemStackTools.setStackSize(itemstack, 0);
-                nnl.set(i, itemstack);;
+                	ItemStackTools.makeEmpty(itemstack);
+                grid.set(i, itemstack);
             }
         }
-        return nnl;
+        return grid;
 	}
 	
 	public ItemStack getRecipeInput() {
