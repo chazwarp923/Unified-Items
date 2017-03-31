1c079c6cf03af6e3c34252b72107638b02f9b6a6
Chaz <chazwarp923@icloud.com>
2017-03-31 14:23:22 -0400

    All storage blocks can now be used in beacons

--- src/main/java/tech/chazwarp923/unifieditems/block/UIBlockStorage.java
+++ src/main/java/tech/chazwarp923/unifieditems/block/UIBlockStorage.java
@@ -5,10 +5,16 @@ package tech.chazwarp923.unifieditems.block;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class UIBlockStorage extends UIBlock {
 	
 	public UIBlockStorage(float hardness, String unlocalizedName, HarvestLevel harvestLevel) {
 		super(Material.IRON, hardness, SoundType.METAL, unlocalizedName, HarvestType.PICKAXE, harvestLevel);
+	}
+	
+	public boolean isBeaconBase(IBlockAccess worldObj, BlockPos pos, BlockPos beacon) {
+		return true;
 	}
 }
