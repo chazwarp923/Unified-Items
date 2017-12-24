345711b3821bc44130c3aa1021d1dae473aa31af
Chaz <chazwarp923@icloud.com>
2017-12-24 01:22:29 -0500

    Fix intellij "organizing" my imoprts
    Signed-off-by: chaz <chazwarp923@icloud.com>

--- src/main/java/tech/chazwarp923/unifieditems/tools/RecipeJsonHelper.java
+++ src/main/java/tech/chazwarp923/unifieditems/tools/RecipeJsonHelper.java
@@ -14,7 +14,13 @@ import tech.chazwarp923.unifieditems.UnifiedItems;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class RecipeJsonHelper {
 	
